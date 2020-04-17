ee0a7e5ac69f7b509957b2d1263aa53a3451c188
Sam <samuel.matsuo.harris@gmail.com>
2020-04-17 12:20:23 +0100

    Merge branch 'basic' of https://github.com/craigmckenziec/Haskell2---Othello
    # Conflicts:
    #	Code/src/Input.hs

--- Code/src/Main.hs
+++ Code/src/Main.hs
@@ -10,23 +10,23 @@ import System.IO.Unsafe
 
 gameLoop :: GameState -> IO ()
 gameLoop st
-    = do if gameOver (board st) == True 
+    = do if gameOver (board st) == True
             then do let ammountWhite = evaluate (board st) White
                     let ammountBlack = evaluate (board st) Black
                     if (ammountBlack > ammountWhite) then putStrLn "Black Wins!"
                     else if (ammountWhite > ammountBlack) then putStrLn "White Wins!"
                     else putStrLn "Draw!"
-            else do if (turn st) == Black 
+            else do if (turn st) == Black
                         then if blackPlayer st == Human
                             then humanGameLoop st
                             else aiGameLoop st
                     else if whitePlayer st == Human
                             then humanGameLoop st
                             else aiGameLoop st
-                
-                    
 
-aiGameLoop :: GameState -> IO ()
+
+
+aiGameLoop :: GameState -> IO (
 aiGameLoop st = do let move = getBestMoveOneDepth (board st) (turn st)
                    let new_board = makeMove (board st) (turn st) (move)
                    if turn st == Black then gameLoop (GameState (fromJust new_board) White (blackPlayer st) (whitePlayer st))
@@ -38,9 +38,9 @@ humanGameLoop st = do putStrLn ("\n" ++ showGameState st)
                       putStr "Move: "
                       move <- getLine
                       if move == "exit" then return ()
-                        else if move == "pass" 
+                        else if move == "pass"
                         then do let currentBoard = board st
-                                let newState = GameState (Board (size currentBoard)  ((passes currentBoard) + 1) (pieces currentBoard)) (other (turn st)) (blackPlayer st) (whitePlayer st) 
+                                let newState = GameState (Board (size currentBoard)  ((passes currentBoard) + 1) (pieces currentBoard)) (other (turn st)) (blackPlayer st) (whitePlayer st)
                                 gameLoop newState
                         else  do
                                 let (x, y) = getCoord move
